fdbd635f2707a77640edc71ae01e30ec2c5f8562
hristo2612 <hristoapps@gmail.com>
2024-09-27 15:27:13 +0300

    Initial setup of MTKView ( Our Metal View )

--- Metal_HelloTriangle/ContentView.swift
+++ Metal_HelloTriangle/ContentView.swift
@@ -1,22 +1,31 @@
-//
-//  ContentView.swift
-//  Metal_HelloTriangle
-//
-//  Created by Hristo on 9/27/24.
-//
-
 import SwiftUI
+import MetalKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ContentView: UIViewRepresentable {
+    
+    func makeCoordinator() -> Renderer {
+        Renderer(self)
     }
+    
+    func makeUIView(context: UIViewRepresentableContext<ContentView>) -> MTKView {
+        let mtkView = MTKView()
+        mtkView.delegate = context.coordinator
+        mtkView.preferredFramesPerSecond = 60
+        mtkView.enableSetNeedsDisplay = true
+        
+        if let metalDevice = MTLCreateSystemDefaultDevice() {
+            mtkView.device = metalDevice
+        }
+        
+        mtkView.framebufferOnly = false
+        mtkView.drawableSize = mtkView.frame.size
+        
+        return mtkView
+    }
+    
+    func updateUIView(_ uiView: MTKView, context: UIViewRepresentableContext<ContentView>) {
+    }
+    
 }
 
 #Preview {
