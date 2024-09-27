78a3ba7ba46d7ea8a2fd305235317c879a5a18f2
hristo2612 <hristoapps@gmail.com>
2024-09-27 14:51:11 +0300

    Initial Commit

---
+++ Metal_HelloTriangle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metal_HelloTriangle
+//
+//  Created by Hristo on 9/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metal_HelloTriangle/Metal_HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_HelloTriangleApp.swift
+//  Metal_HelloTriangle
+//
+//  Created by Hristo on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Metal_HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
